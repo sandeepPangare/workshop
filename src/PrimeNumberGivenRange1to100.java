9ed217e0811e2babc3a288ca24620b8a16a8ce5c
sandeepPangare <pangaresandeep96@gmail.com>
2022-09-05 17:53:07 +0530

    added:Prime_Number_Given+Range_1to100

---
+++ src/PrimeNumberGivenRange1to100.java
@@ -0,0 +1,23 @@
+public class PrimeNumberGivenRange1to100 {
+    public static void main(String[] args) {
+
+        int temp=0;
+
+        for (int i=1; i<=100; i++){
+            for (int j=2; j<=i-1; j++){
+                if (i%j==0){
+                    temp=temp+1;
+                }
+            }
+            if (temp==0){
+                System.out.println(i + " is Prime Number");
+            }
+            else {
+                    temp=0;
+                System.out.println(i + " is Not Prime Number");
+            }
+        }
+
+
+    }
+}
